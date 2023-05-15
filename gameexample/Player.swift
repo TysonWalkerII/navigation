93aa482a7dd50d9d031c70e3d72cca5ae2817458
Jcodes25 <122479619+Jcodes25@users.noreply.github.com>
2023-05-15 14:38:11 -0400

    Created player struct

---
+++ gameexample/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  gameexample
+//
+//  Created by Jordan Barconey on 5/15/23.
+//
+
+struct player {
+    var health = 20
+    var str = 10
+    var cha = 10
+    var int = 10
+    var lck = 10
+}
