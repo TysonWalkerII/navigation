49c3980194a31b1d45264f4afe5a95a7be41465e
Jcodes25 <122479619+Jcodes25@users.noreply.github.com>
2023-05-16 14:49:40 -0400

    Finished animation and created a preview provider for the SKScene

--- gameexample/Prep/AnimationScene.swift
+++ gameexample/Prep/AnimationScene.swift
@@ -7,6 +7,7 @@
 
 import SpriteKit
 import GameplayKit
+import SwiftUI
 
 class AnimationScene: SKScene {
     var `guard` = SKSpriteNode()
@@ -16,7 +17,44 @@ class AnimationScene: SKScene {
     
     override func didMove(to view: SKView) {
         
+        guardAtlas = SKTextureAtlas(named: "guard")
         
         
+        for i in 1...guardAtlas.textureNames.count {
+            
+            let name = "dance_\(i).png"
+            guardArray.append(SKTexture(imageNamed: name))
+        }
+        
+        `guard` = SKSpriteNode(imageNamed: "dance_1")
+        `guard`.position = CGPoint(x:275, y:275)
+        
+        addChild(`guard`)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        `guard`.run(SKAction.repeatForever(SKAction.animate(with: guardArray, timePerFrame: 0.1)))
+    }
+}
+
+struct SKContentView: View {
+    var scene: SKScene {
+        let scene = AnimationScene()
+        scene.size = CGSize(width:500, height: 500)
+        scene.scaleMode = .fill
+        return scene
+    }
+    
+    var body: some View {
+        SpriteView(scene: scene)
+            .frame(width: 600, height: 600)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct AnimationScene_Previews: PreviewProvider {
+    static var previews: some View {
+        SKContentView()
     }
 }
