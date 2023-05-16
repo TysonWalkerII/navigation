7df026957c66d3fb90c98f7694ceda7a1f5351f1
Jcodes25 <122479619+Jcodes25@users.noreply.github.com>
2023-05-16 14:18:18 -0400

    started guard animation. added tester animation assests

---
+++ gameexample/Prep/AnimationScene.swift
@@ -0,0 +1,22 @@
+//
+//  AnimationScene.swift
+//  gameexample
+//
+//  Created by Jordan Barconey on 5/16/23.
+//
+
+import SpriteKit
+import GameplayKit
+
+class AnimationScene: SKScene {
+    var `guard` = SKSpriteNode()
+    
+    var guardAtlas = SKTextureAtlas()
+    var guardArray = [SKTexture]()
+    
+    override func didMove(to view: SKView) {
+        
+        
+        
+    }
+}
