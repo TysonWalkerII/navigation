dc1aa2d54a3a32638fe57c5e6a5d9c230312a2c6
Jcodes25 <122479619+Jcodes25@users.noreply.github.com>
2023-05-31 16:39:06 -0400

    Added view model function move and refactored Items and Rooms

--- gameexample/Models/Item.swift
+++ gameexample/Models/Item.swift
@@ -8,21 +8,26 @@
 import Foundation
 
 // TODO: Make struct
-class Item :Identifiable{
+//struct Item : Identifiable{
+struct Item {
     
-    
-    let id = UUID()
+//    let id = UUID()
     public var itemImg:String?
     public var itemName:String?
     public var itemDescription:String?
-     
-    init(itemImg: String? = nil, itemName: String? = nil, itemDescription: String? = nil) {
-        self.itemImg = itemImg
-        self.itemName = itemName
-        self.itemDescription = itemDescription
-    }
+
+    
+    
+//    init(itemImg: String? = nil, itemName: String? = nil, itemDescription: String? = nil) {
+//        self.itemImg = itemImg
+//        self.itemName = itemName
+//        self.itemDescription = itemDescription
+//    }
+    
+
     
    
     
-    
+    static let items: [Item] = [Item(itemImg: "Bread", itemName: "Bread", itemDescription: "It's just Bread..."), Item(itemImg: "coffeecup", itemName: "Koffee", itemDescription: "Get energized with our new invention... A Cup O' Joe"), Item(itemImg: "", itemName: "here", itemDescription: ""), Item(itemImg: "", itemName: "we", itemDescription: ""), Item(itemImg: "", itemName: "go", itemDescription: ""), Item(itemImg: "", itemName: "son", itemDescription: ""), Item(itemImg: "", itemName: "yep", itemDescription: "")]
 }
+
