3b3486b50fa5309c1bd68fa3a631654df20c21f2
Tyson Walker II <tywalk10@gmail.com>
2023-06-06 12:37:15 -0400

    fixed the inventory part of the ui

--- gameexample/Views/InventoryView.swift
+++ gameexample/Views/InventoryView.swift
@@ -14,10 +14,23 @@ struct InventoryView: View {
     var body: some View {
         
         Section{
-            ZStack{
+            ZStack(alignment: Alignment(horizontal: .center, vertical: .center)){
                 InventoryBoxView()
-                    .offset(x: 25, y:30)
+                   // .offset(x: 25, y:30)
+               
                     VStack{
+                        HStack{
+                            Spacer().frame(width:250)
+                            Button{
+                                print("changing the boolean")
+                                vm.showInventory.toggle()
+                            } label:{
+                                Image(systemName: "multiply.circle").foregroundColor(.white)
+                           
+                            }
+                            
+                          
+                        }
                         HStack {
                             Text("INVENTORY").font(.custom("ChakraPetch-Bold", size: 21)).padding().foregroundColor(.white)
                             Text("HISTORY").font(.custom("ChakraPetch-Bold", size: 21)).padding().foregroundColor(.white)
@@ -30,7 +43,7 @@ struct InventoryView: View {
                                         
                                         tappedOnItem = vm.player.inventory[thing]
                                         print(tappedOnItem.itemImg)
-                                        //                                        if vm.currentRoom.key?.itemName == vm.player.inventory[thing].itemName{
+                                        //  if vm.currentRoom.key?.itemName == vm.player.inventory[thing].itemName{
                                         //                                            vm.trash.itemsInTrash.append(vm.player.inventory[thing])
                                         //                                            vm.player.inventory.remove(at: thing)
                                         //                                            vm.currentRoom.locked.toggle()
@@ -55,8 +68,9 @@ struct InventoryView: View {
                             } .padding(.horizontal)
                             
                             
-                        }.frame(maxWidth:300,maxHeight: 300)
+                        }.frame(width:300,height: 300)
                         VStack{
+                            
                             HStack{
                                 Image("\(tappedOnItem.itemImg ?? "")").resizable().scaledToFit()
                                     .border(.white)
@@ -83,7 +97,8 @@ struct InventoryView: View {
                         
                         
                     }
-                }
+                
+            }
             }
         } 
     }
